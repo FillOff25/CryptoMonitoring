efa95616e5af32c449ae9c36c32e9c544a8326b6
FillOff25 <minkovalexandr2006@gmail.com>
2025-07-15 11:57:57 +0300

    add serilog to DataGenerator service, add healthchecking of postgres container

--- src/CryptoMonitoring.DataGenerator/CryptoMonitoring.DataGenerator.DataAccess/DependencyInjection.cs
+++ src/CryptoMonitoring.DataGenerator/CryptoMonitoring.DataGenerator.DataAccess/DependencyInjection.cs
@@ -6,31 +6,40 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace CryptoMonitoring.DataGenerator.DataAccess;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddRepositories(this IServiceCollection services)
+    public static IServiceCollection AddDataAccess(this IServiceCollection services, IConfiguration configuration)
     {
+        // Register DbContext in DI, configure and map custom enum types in database
+        services.AddDbContext<CryptoMonitoringDataDbContext>(options =>
+            options.UseNpgsql(configuration["CRYPTO_MONITORING_DATA_DB_CONNECTION_STRING"],
+                o => o.MapEnum<IndicatorTypeEnum>("indicator_type_enum")));
+
+        // Register repositories in DI
         services.AddScoped(typeof(IGenericRepository<,>), typeof(GenericRepository<,>));
 
         return services;
     }
 
-    public static IServiceCollection AddDatabases(this IServiceCollection services, IConfiguration configuration)
-    {
-        services.AddDbContext<CryptoMonitoringDataDbContext>(options =>
-            options.UseNpgsql(configuration["CRYPTO_MONITORING_DATA_DB_CONNECTION_STRING"],
-                o => o.MapEnum<IndicatorTypeEnum>("indicator_type_enum")));
-
-        return services;
-    }
-
+    // Extension method for applying migrations in database
     public static async Task ApplyMigrationsAsync(this IHost app)
     {
-        using var scope = app.Services.CreateScope();
-        var dbContext = scope.ServiceProvider.GetRequiredService<CryptoMonitoringDataDbContext>();
-        await dbContext.Database.MigrateAsync();
+        try
+        {
+            using var scope = app.Services.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<CryptoMonitoringDataDbContext>();
+            await dbContext.Database.MigrateAsync();
+
+            Log.Information("Database migrations applied successfully");
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "An error occurred while applying database migrations");
+            throw;
+        }
     }
 }
--- src/CryptoMonitoring.DataGenerator/CryptoMonitoring.DataGenerator.Presentation/Program.cs
+++ src/CryptoMonitoring.DataGenerator/CryptoMonitoring.DataGenerator.Presentation/Program.cs
@@ -1,30 +1,52 @@
 using CryptoMonitoring.DataGenerator.DataAccess;
+using Serilog;
+using Serilog.Events;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Default", LogEventLevel.Information)
+    .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning)
+    .WriteTo.Console()
+    .CreateLogger();
 
-DotNetEnv.Env.Load();
-builder.Configuration.AddEnvironmentVariables();
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddDatabases(builder.Configuration);
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    Log.Information("Starting web application");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    DotNetEnv.Env.Load();
+    builder.Configuration.AddEnvironmentVariables();
+
+    builder.Services.AddSerilog();
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    builder.Services.AddDataAccess(builder.Configuration);
+
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    await app.ApplyMigrationsAsync();
+
+    app.UseSerilogRequestLogging();
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
+
+    app.Run();
 }
-
-await app.ApplyMigrationsAsync();
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    await Log.CloseAndFlushAsync();
+}
